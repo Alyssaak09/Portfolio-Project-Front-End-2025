d8c980b520ae3e85b5b166ac57c55b83143a18f7
Alyssa <=>
2025-08-17 16:28:07 -0400

    add picture in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,7 +26,9 @@ function About() {
             <section id="about">
                 <h2>About</h2>
 
-                {aboutContent}
+                <img src="./images/alyssapicture.png" alt="picture of alyssa" />
+
+               <p>{aboutContent}</p> 
 
             </section>
 
