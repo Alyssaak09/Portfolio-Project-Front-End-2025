54aefeea4df3728a87c000efa9ee2bfb55e8188b
Alyssa <=>
2025-08-17 10:21:43 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+import './App.css';
+
+function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchProjects() {
+      let resp = await fetch('https://portfolio-project-2025.onrender.com/api/projects');
+      let data = await resp.json();
+
+      console.log(data);
+      setProjects(data);
+
+    }  // fetchProjects()
+
+    // Call the fetchProjects function to get the projects data
+    fetchProjects();
+
+  }, []);  // useEffect(() 
+
+
+  return (
+    <ul>
+      {
+      projects.map(p => {
+        return (
+          <li key={p.id}>
+            {p.title}            
+          </li>
+        )
+
+      })
+     }
+    </ul>
+  )
+}
+
+export default App;
+
+
